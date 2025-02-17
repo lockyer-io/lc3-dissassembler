65cb8aeaf8f622a505d7a8aa7931b7e6512c0593
Eddie Lockyer <eddielockyer@Eddies-MacBook-Pro.local>
2025-02-17 12:09:04 +1000

    Finished disassembly logic, still contains error with BR vs unused memory

--- src/lc3-disassembler.c
+++ src/lc3-disassembler.c
@@ -54,6 +54,14 @@ enum
     TRAP_SLEEP = 0x29,  /* pause for a specified delay */
 };
 
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
 // Read .obj file into memory
 void read_obj_file(const char *filename) {
     FILE *file = fopen(filename, "rb");
@@ -80,6 +88,8 @@ void read_obj_file(const char *filename) {
 
 void disassemble_instruction(uint16_t addr, uint16_t instr) {
     uint16_t op = instr >> 12;
+
+    printf("0x%04X: ", addr);
 
     switch (op)
         {
@@ -120,7 +130,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                 {
                     uint16_t r0 = (instr >> 9) & 0x7;
                     uint16_t r1 = (instr >> 6) & 0x7;
-                    printf("NOT R%d, R%d", r0, r1);
+                    printf("NOT R%d, R%d\n", r0, r1);
                 }
                 break;
             case OP_BR:
@@ -134,7 +144,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     if (cond_flag & 0x2) printf("z");  // Zero
                     if (cond_flag & 0x1) printf("p");  // Positive
 
-                    printf(" 0x%04X\n", target_addr);
+                    printf(" %#06x\n", target_addr);
                 }
                 break;
             case OP_JMP:
@@ -155,7 +165,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     {
                         uint16_t pc_offset = sign_extend(instr & 0x7FF, 11); 
                         uint16_t target_addr = addr + 1 + pc_offset;  // Calculate jump target
-                        printf("JSR %s\n", target_addr);  // Print label
+                        printf("JSR %#06x\n", target_addr);  // Print label
                     } else {
                         uint16_t r1 = (instr >> 6) & 0x7;
                         printf("JSRR R%d\n", r1);
@@ -175,7 +185,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     uint16_t r0 = (instr >> 9) & 0x7;
                     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
                     uint16_t target_addr = addr + 1 + pc_offset;
-                    printf("LDI R%d, %s\n", r0, target_addr);
+                    printf("LDI R%d, %#06x\n", r0, target_addr);
                 }
                 break;
             case OP_LDR:
@@ -191,7 +201,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     uint16_t r0 = (instr >> 9) & 0x7;
                     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
                     uint16_t target_addr = addr + 1 + pc_offset;
-                    printf("LEA R%d, %s\n", r0, target_addr);
+                    printf("LEA R%d, %#06x\n", r0, target_addr);
                 }
                 break;
             case OP_ST:
@@ -199,7 +209,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     uint16_t r0 = (instr >> 9) & 0x7;
                     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
                     uint16_t target_addr = addr + 1 + pc_offset;
-                    printf("ST R%d, %s\n", r0, target_addr);
+                    printf("ST R%d, %#06x\n", r0, target_addr);
                 }
                 break;
             case OP_STI:
@@ -207,7 +217,7 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                     uint16_t r0 = (instr >> 9) & 0x7;
                     uint16_t pc_offset = sign_extend(instr & 0x1FF, 9);
                     uint16_t target_addr = addr + 1 + pc_offset;
-                    printf("STI R%d, %s\n", r0, target_addr);
+                    printf("STI R%d, %#06x\n", r0, target_addr);
                 }
                 break;
             case OP_STR:
@@ -248,11 +258,58 @@ void disassemble_instruction(uint16_t addr, uint16_t instr) {
                 break;
             case OP_RES:
                 {
-                    printf("RES");
+                    printf("RES\n");
                 }
+                break;
             case OP_RTI:
                 {
-                    printff("RTI");
+                    printf("RTI\n");
                 }
+                break;
         }
+    fflush(stdout);
+}
+
+void disassemble_program(uint16_t origin, size_t num_instructions) {
+    printf("Disassembly Output:\n\n");
+
+    for (size_t i = 0; i < num_instructions; i++) {
+        uint16_t addr = origin + i;
+        uint16_t instr = memory[addr];
+
+        disassemble_instruction(addr, instr);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <file.obj>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* file = fopen(argv[1], "rb");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Read origin address
+    uint16_t origin;
+    fread(&origin, sizeof(uint16_t), 1, file);
+    origin = swap16(origin);
+
+    // Load instructions into memory from the origin
+    uint16_t *p = memory + origin;
+    size_t read_count = fread(p, sizeof(uint16_t), MEMORY_MAX - origin, file);
+    for (size_t i = 0; i < read_count; i++) {
+        p[i] = swap16(p[i]);  // Swap endianness
+    }
+
+    fclose(file);
+
+    // Disassemble the program
+    disassemble_program(origin, read_count);
+
+    return 0;
 }
